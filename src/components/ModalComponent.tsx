239ad81c0057eee1c5be6d6af2f31657e1ece3fc
Andre Assunção <104846613+AndreAssun@users.noreply.github.com>
2023-11-23 15:18:43 -0300

    atualização modal

--- src/components/ModalComponent.tsx
+++ src/components/ModalComponent.tsx
@@ -2,27 +2,33 @@ import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import Modal from 'react-native-modal';
 
-const ModalComponent = ({ modalVisible, closeModal, modalContent }) => {
+const ModalComponent = ({ isVisible, closeModal }) => {
   const handleButtonPress = () => {
     closeModal();
   };
 
   return (
     <Modal
-      isVisible={modalVisible}
+      isVisible={isVisible}
       style={styles.modal}
       animationIn="slideInDown"
       animationOut="slideOutUp"
       backdropOpacity={0.5}
     >
       <View style={styles.modalContent}>
-        <TouchableOpacity onPress={handleButtonPress} style={styles.closeButton}>
-          <Text>X</Text>
-        </TouchableOpacity>
-        {modalContent}
-        <TouchableOpacity onPress={handleButtonPress} style={styles.modalButton}>
-          <Text>Aplicar</Text>
-        </TouchableOpacity>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Solicitações</Text>
+          <TouchableOpacity onPress={handleButtonPress} style={styles.closeButton}>
+            <Text>X</Text>
+          </TouchableOpacity>
+        </View>
+        
+        <View style={styles.content}>
+          <Text>Conteúdo do Modal</Text>
+          <TouchableOpacity onPress={handleButtonPress} style={styles.modalButton}>
+            <Text style={styles.modalButtonText}>aplicar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </Modal>
   );
@@ -31,7 +37,7 @@ const ModalComponent = ({ modalVisible, closeModal, modalContent }) => {
 const styles = StyleSheet.create({
   modal: {
     width: '90%',
-    height: '10%',
+    height: '30%',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -43,20 +49,42 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  header: {
+    backgroundColor: '#00ADB5',
+    padding: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  headerText: {
+    color: 'white',
+    marginRight: 180,
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   closeButton: {
-    position: 'absolute',
-    top: 10,
-    right: 10,
+    color: 'white',
     padding: 10,
     borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
   },
   modalButton: {
-    marginTop: 10,
+    position: 'absolute',
+    bottom: 0,
+    right: -90,
     padding: 10,
-    backgroundColor: 'green',
+    backgroundColor: '#00ADB5',
     borderRadius: 4,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalButtonText: {
+    color: 'white',
   },
 });
 
